4fea4a77751b3227d1c991e0116c7e8fb0b185e8
gnagypal <gnagypal@gmail.com>
2015-08-24 21:01:47 +0200

    1

---
+++ AnkiTTSGenerator/Configuration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnkiTTSGenerator
+{
+    enum ProgramFunction
+    {
+        ListInstalledVoices,
+        GenerateTTS
+    }
+
+    class Configuration
+    {
+        public ProgramFunction Function { get; set; }
+
+        public string SelectedVoice { get; set; }
+
+        public int MP3BitRate { get; set; }
+
+        public string SourceXmlFileName { get; set; }
+        
+        public string RowElementName { get; set; }
+
+        public string IDElementName { get; set; }
+
+        public List<string> TextElementNames { get; set; }
+
+        public string Mp3FilePrefix { get; set; }
+
+        public string DestinationDirectory { get; set; }
+    }
+}
